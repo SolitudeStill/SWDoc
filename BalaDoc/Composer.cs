b1780ed0e4b4d9c1834c9f43d01b319dfb7f4269
hissyu <hissyu@hotmail.com>
2020-09-10 17:19:11 +0800

    Add some small things (about version and initialization)

--- BalaDoc/Composer.cs
+++ BalaDoc/Composer.cs
@@ -10,6 +10,7 @@ using YamlDotNet.Serialization;
 using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
 using System.Security.Cryptography;
+using Markdig.Syntax;
 
 namespace BalaDoc
 {
@@ -25,10 +26,54 @@ namespace BalaDoc
 
         public string Hash { get; set; }
     }
+    public enum VerTestResult
+    {
+        Old, Current, Mirainokoto, Uninitialized
+    }
     class Composer
     {
-        private const int version = 0;
+        private readonly int version = 0;
+        private readonly string WorkDir;
+        #region Directories
+        private static readonly string _DirSourceDoc = "doc";
+        private static readonly string _DirTemplate = "template";
+        private static readonly string _DirTemplateDefault = $"{_DirTemplate}/Default";
+        private static readonly string _DirMeta = "./inf";
+        private static readonly string _DirMetaFileTracking = $"{_DirMeta}/track";
+        private static readonly string _DirCategories = "category";
+        private static readonly string _DirDestPages = "pages";
 
+        private static readonly string _FileMetaSerialNum = $"{_DirMeta}/sn";
+        #endregion
+
+        public Composer(string WorkDir)
+        {
+            WorkDir = Path.GetDirectoryName(WorkDir);
+        }
+        public static VerTestResult TestVersion(Composer c)
+        {
+            var version = c.GetCurrentVersion();
+            return version switch
+            {
+                int when version > c.version => VerTestResult.Mirainokoto,
+                int when version == c.version => VerTestResult.Current,
+                int when version < c.version => VerTestResult.Old,
+                _ => VerTestResult.Uninitialized
+            };
+        }
+        public static void CurveVersion(Composer c)
+        {
+            File.WriteAllBytes(Path.Combine(c.WorkDir, _FileMetaSerialNum), BitConverter.GetBytes(c.version));
+        }
+        public static void CreateDirs(string baseDir, string[] dirs)
+        {
+            var dir = Path.GetDirectoryName(baseDir);
+
+            foreach (var d in dirs)
+            {
+                Directory.CreateDirectory(Path.Combine(dir, d));
+            }
+        }
         public static PostFront GetFront(string content)
         {
             var yamlDeserializer = new DeserializerBuilder()
@@ -46,7 +91,6 @@ namespace BalaDoc
 
             return front;
         }
-
         public static byte[] GetBytes(string source)
         {
             return Encoding.UTF8.GetBytes(source);
@@ -62,6 +106,19 @@ namespace BalaDoc
             return sb.ToString();
         }
 
+        public int? GetCurrentVersion()
+        {
+            byte[] bs;
+            try
+            {
+                bs = File.ReadAllBytes(Path.Combine(WorkDir, _FileMetaSerialNum));
+            }
+            catch (FileNotFoundException)
+            {
+                return null;
+            }
+            return BitConverter.ToInt32(bs);
+        }
         public void ProcessDoc(string filename)
         {
             var pipeline = new MarkdownPipelineBuilder()
@@ -72,7 +129,7 @@ namespace BalaDoc
 
             var content = File.ReadAllText(filename);
             PostFront front = GetFront(content);
-            front.Hash = GetHash(content); // this should be done before the function, later do it
+            front.Hash = GetHash(content); // TODO: this should be done before the function, later do it
 
             if (front.Date == null)
                 front.Date = File.GetCreationTime(filename);
@@ -80,19 +137,31 @@ namespace BalaDoc
 
             var doc = Markdown.Parse(content, pipeline);
 
-            foreach(var block in doc)
+            // Use the first leaf block (containing plain text) content as the title
+            // 80 character at maximum
+            foreach (var block in doc)
             {
-                if(block is Markdig.Syntax.HeadingBlock b && b.Level == 1)
+                if(block is LeafBlock b)
                 {
-                    front.Title = (b.Inline.FirstChild as Markdig.Syntax.Inlines.LiteralInline).Content.ToString();
-
-                    
+                    front.Title = (b.Inline.FirstChild as Markdig.Syntax.Inlines.LiteralInline)?.Content.ToString();
+                    if (front.Title.Length > 80)
+                        front.Title = front.Title.Substring(0, 80);
+                    break;
                 }
             }
-
+            
             var result = Markdown.ToHtml(content, pipeline);
 
-            File.WriteAllText(Path.Combine(Path.GetDirectoryName(filename), "gen.html"), result);
+            // the directory will be based on "CurrentDirectory"
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(filename), "../page", "gen.html"), result);
+        }
+
+        public void InitDir(string route)
+        {
+            string[] dirs = { _DirMeta, _DirMetaFileTracking, _DirSourceDoc, _DirTemplate, _DirTemplateDefault, _DirCategories, _DirDestPages };
+            CreateDirs(WorkDir,dirs);
+
+            CurveVersion(this);
         }
     }
 }
